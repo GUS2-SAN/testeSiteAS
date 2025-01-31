87147e4743c1e8c846aa9c336e1cbe0a61b3ff17
Augusto Santana <102883345+GUS2-SAN@users.noreply.github.com>
2025-01-30 23:09:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    // Aqui você pode adicionar código para enviar os dados do formulário para um servidor, por exemplo.
+});
